fc9c076d8eecb8f56b7d89614489575324ecae3b
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-05-08 21:59:25 +0500

    commmit

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/+_zNMxMi7cHplYmMy';
+    window.location.href = 'https://arabbaevna-one.netlify.app/';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
